701fa5bcf97c42c9cd4e62de640bcb9a54e9edd0
aviroopjana <aviroopjana@gmail.com>
2024-03-03 00:14:32 +0530

    Update package.json and index.ts

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,11 @@ app.listen(port, () => {
 });
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING)
+  .connect(process.env.MONGO_CONNECTION_STRING as string)
   .then(() => {
     console.log("Connected to MongoDB Database");
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB", err);
   });
+

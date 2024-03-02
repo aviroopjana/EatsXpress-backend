c7e642c5dcd9865913be4d0007054111532531ae
aviroopjana <aviroopjana@gmail.com>
2024-03-02 20:07:30 +0530

    connect to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
+import cors from "cors";
 import express, { Application, Express, Request, Response } from "express";
+import mongoose, { Mongoose } from "mongoose";
+import dotenv from 'dotenv';
 
 const app: Application = express();
 
+dotenv.config();
+
 const port = 3000;
 
-app.get('/',(req: Request, res: Response) => {
-    res.send('Server started');
-})
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/test', (req, res) => {
-    res.send('Test Server running successfully')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server started");
+});
+
+app.get("/api/test", (req, res) => {
+  res.send("Test Server running successfully");
+});
 
 app.listen(port, () => {
-    console.log(`Server started on ${port}`);
-})
+  console.log(`Server started on ${port}`);
+});
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB Database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });

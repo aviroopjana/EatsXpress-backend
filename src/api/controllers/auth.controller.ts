fe151ea95e3e6c860db65457fb846dc5d55f7a00
aviroopjana <aviroopjana@gmail.com>
2024-03-03 03:25:55 +0530

    feat: Implement signin API route with jwt token

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -1,25 +1,25 @@
-import { Request, Response, NextFunction } from 'express';
-import User from '../../models/user.model';
-import { errorHandler } from '../../utils/error';
-import bcryptjs from 'bcryptjs';
+import { Request, Response, NextFunction } from "express";
+import User from "../../models/user.model";
+import { errorHandler } from "../../utils/error";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 type UserRequestBody = {
-    username: string;
-    email: string;
-    password: string;
-    name: string;
-    phoneNumber: string;
-    profilePicture: string;
+  username: string;
+  email: string;
+  password: string;
+  name: string;
+  phoneNumber: string;
+  profilePicture: string;
 };
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
-  const {
-    username,
-    email,
-    password,
-    name,
-    phoneNumber
-  }: UserRequestBody = req.body;
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, email, password, name, phoneNumber }: UserRequestBody =
+    req.body;
 
   if (
     !username ||
@@ -36,7 +36,7 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
     return next(errorHandler(res, 400, "All fields are required!"));
   }
 
-  const hashedPassword = bcryptjs.hashSync('password', 10);
+  const hashedPassword = bcryptjs.hashSync("password", 10);
 
   const newUser = new User({
     username,
@@ -48,16 +48,46 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
 
   try {
     await newUser.save();
-    res.status(201).json('New user saved to database');
+    res.status(201).json("New user saved to database");
   } catch (error) {
     next(error);
   }
 };
 
-export const signin = async (req: Request, res: Response, next: NextFunction) => {
-  // Implement signin logic here
+export const signin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password }: UserRequestBody = req.body;
+
+  if (!username || !password || username === "" || password === "") {
+    return next(errorHandler(res, 401, "All fields are required"));
+  }
+
   try {
-    // To Do signin logic
+    const validUser = await User.findOne({ username });
+
+    if (!validUser) {
+      return next(errorHandler(res, 404, "User not found"));
+    }
+
+    const token = jwt.sign(
+      {
+        username: validUser.username,
+        password: validUser.password,
+      },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser.toJSON();
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
